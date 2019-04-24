0f43db2153069a589bb4da78b837c173e51de3e4
Dean Raspa <draspa@ebsco.com>
2019-04-24 10:59:03 -0400

    Added .. aliases up to 5

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -7,6 +7,15 @@ alias nano='nano -c'
 alias -s log=nano
 alias -s conf=nano
 
+## a quick way to get out of current directory ##
+alias ..='cd ..'
+alias ...='cd ../../../'
+alias ....='cd ../../../../'
+alias .....='cd ../../../../'
+alias .4='cd ../../../../'
+alias .5='cd ../../../../..'
+
+# AWS CLI
 alias aws-plive="unset AWS_PROFILE && gimme-aws-creds --profile eis-platformlive && export AWS_PROFILE=eis-platformlive"
 alias aws-live="unset AWS_PROFILE && gimme-aws-creds --profile eis-deliverylive && export AWS_PROFILE=eis-deliverylive"
 alias aws-int="unset AWS_PROFILE && gimme-aws-creds --profile eis-deliveryintegration && export AWS_PROFILE=eis-deliveryintegration"
