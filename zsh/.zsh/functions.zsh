2b591fc27cb456e84d56dac14819e81b2e756c32
Dean Raspa <draspa@ebsco.com>
2019-05-22 11:41:28 -0400

    removed auto tmux on ssh

--- zsh/.zsh/functions.zsh
+++ zsh/.zsh/functions.zsh
@@ -17,9 +17,9 @@ where() {
     find . -name \*$1\*
 }
 
-ssh() {
-	/usr/bin/ssh -t "$@" tmux new-session -A -s fabrice-session
-}
+# ssh() {
+# 	/usr/bin/ssh -t "$@" tmux new-session -A -s fabrice-session
+# }
 
 psql_history() {
     psql -U postgres -c "SELECT (pg_stat_file('base/'||oid ||'/PG_VERSION')).modification, datname FROM pg_database;"
