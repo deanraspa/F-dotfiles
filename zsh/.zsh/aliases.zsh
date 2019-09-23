0c5f11a45db848607062afe69f006cb39df4e258
Dean Raspa <draspa@ebsco.com>
2019-09-23 10:25:27 -0400

    Added lz

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -9,8 +9,8 @@ alias -s conf=nano
 
 ## a quick way to get out of current directory ##
 alias ..='cd ..'
-alias ...='cd ../../../'
-alias ....='cd ../../../../'
+alias ...='cd ../..'
+alias ....='cd ../../..'
 alias .....='cd ../../../../'
 alias .4='cd ../../../../'
 alias .5='cd ../../../../..'
@@ -20,8 +20,10 @@ alias aws-plive="unset AWS_PROFILE && gimme-aws-creds --profile eis-platformlive
 alias aws-live="unset AWS_PROFILE && gimme-aws-creds --profile eis-deliverylive && export AWS_PROFILE=eis-deliverylive"
 alias aws-int="unset AWS_PROFILE && gimme-aws-creds --profile eis-deliveryintegration && export AWS_PROFILE=eis-deliveryintegration"
 alias aws-dqa="unset AWS_PROFILE && gimme-aws-creds --profile eis-deliverydevqa && export AWS_PROFILE=eis-deliverydevqa"
+alias aws-lz-dqa="unset AWS_PROFILE && gimme-aws-creds --profile eis-lz-ehost-devqa && export AWS_PROFILE=eis-lz-ehost-devqa"
 alias aws-lo="unset AWS_PROFILE"
 
 alias ecr-login='aws-plive && eval $(aws ecr get-login --no-include-email)'
+alias ecr-lz-login='aws-lz-dqa && eval $(aws ecr get-login --no-include-email)'
 
 [ -f ~/.kubectl_aliases ] && source ~/.kubectl_aliases
